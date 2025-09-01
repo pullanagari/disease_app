d1d30d3fc51dfac205f4ef76783cf165ec9f304c
Reddy pullanagari <greensreddy@gmail.com>
2025-09-01 18:58:11 +0930

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def reload_data():
 hide_code = """
     <style>
     # .st-emotion-cache-1avcm0n {visibility: hidden;}
-    # header {visibility: hidden;}
+    header {visibility: hidden;}
     footer {visibility: hidden;}
     </style>
 """
@@ -312,6 +312,7 @@ else:
     - If data doesn't update automatically, try refreshing the page
     """
     )
+
 
 
 
