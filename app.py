9e04f7c32e313dd6568d05233171ed9660204fac
Reddy pullanagari <greensreddy@gmail.com>
2025-09-02 20:21:04 +0930

    Update app.py

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ def reload_data():
 hide_code = """
     <style>
    
-    # [data-testid="stMainMenu"] {display: none;}
-    # [data-testid="stToolbar"] {display: none;}
+    [data-testid="stMainMenu"] {display: none;}
+    [data-testid="stToolbar"] {display: none;}
     footer {visibility: hidden;}
 
     
@@ -350,6 +350,7 @@ else:
     - If data doesn't update automatically, try refreshing the page
     """
     )
+
 
 
 
