ef9c8400806c11e5ff3d7561af2014b253c05be4
Reddy pullanagari <greensreddy@gmail.com>
2025-09-02 20:41:25 +0930

    Update app.py

--- app.py
+++ app.py
@@ -65,11 +65,11 @@ def reload_data():
 # Hide Streamlit default UI elements
 hide_code = """
     <style>
-    [data-testid="stMainMenu"] {display: none;}
+    # [data-testid="stMainMenu"] {display: none;}
     [data-testid="stToolbar"] {display: none;}
     footer {visibility: hidden;}
     .st-emotion-cache-1avcm0n {visibility: hidden;}
-    MainMenu {visibility: hidden;}
+    # MainMenu {visibility: hidden;}
     header {visibility: hidden;}
     footer {visibility: hidden;}
     </style>
@@ -347,6 +347,7 @@ else:
     - If data doesn't update automatically, try refreshing the page
     """
     )
+
 
 
 
