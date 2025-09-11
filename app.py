baf4508c7987b62ef447342b136e3480582f384b
Reddy pullanagari <greensreddy@gmail.com>
2025-09-11 20:30:52 +0930

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,12 @@ sidebar_mobile_friendly = """
 }
 </style>
 """
+hide_menu_style = """
+    <style>
+        #MainMenu {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.markdown(sidebar_mobile_friendly, unsafe_allow_html=True)
 
 st.sidebar.markdown("## 🌾 South Australia Disease Surveillance")
@@ -463,6 +469,7 @@ elif menu == "Resources":
         - [SARDI Biosecurity](https://pir.sa.gov.au/sardi/crop_sciences/plant_health_and_biosecurity)
         """
     )
+
 
 
 
